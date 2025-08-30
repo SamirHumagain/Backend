2275b8c51dcd5bceabee04b7a853f3dc28782a3e
Samir Humagain <hmgsamir08@gmail.com>
2025-08-30 09:58:26 +0545

    deleted venue

--- rating/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path
-from .views import VenueRatingCreateView, VenueRatingListView
-
-urlpatterns = [
-    path('add/', VenueRatingCreateView.as_view(), name='add-rating'),
-    path('venue/<int:venue_id>/', VenueRatingListView.as_view(), name='venue-ratings'),
-]
